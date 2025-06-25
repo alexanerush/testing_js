c9064064b30d863415772da94ea578bdbd2bef5a
Aliaksandra Nerush <aliaksandra.nerush@student.ehu.lt>
2025-06-25 17:51:35 +0300

    fix: explicitly set supportFile for mochawesome reporter

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,13 +1,19 @@
-// cypress.config.mjs
 import { defineConfig } from 'cypress';
 import mochawesome from 'cypress-mochawesome-reporter/plugin.js';
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome',
+    overwrite: false,
+    html: true,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
       mochawesome(on);
       return config;
     },
-    baseUrl: 'https://demoqa.com',
-  },
+    supportFile: 'cypress/support/e2e.js',
+  },  
 });
