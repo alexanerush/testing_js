a88647d1b0c40deb3b6b55b1cd4f8250926c16c3
Aliaksandra Nerush <aliaksandra.nerush@student.ehu.lt>
2025-06-28 00:24:35 +0300

    fix: define globals for Node.js and Cypress in ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,26 @@
-import js from '@eslint/js';
-import eslintPluginImport from 'eslint-plugin-import';
-import eslintPluginCypress from 'eslint-plugin-cypress';
-
-/** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      globals: {
-        require: 'readonly',
-        module: 'readonly',
-        describe: 'readonly',
-        it: 'readonly',
-        expect: 'readonly',
-        beforeEach: 'readonly',
-        cy: 'readonly',
-        Cypress: 'readonly',
+    {
+      files: ['**/*.js'],
+      languageOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        globals: {
+          console: 'readonly',
+          process: 'readonly',
+          module: 'readonly',
+          require: 'readonly',
+          __dirname: 'readonly',
+          cy: 'readonly',
+          Cypress: 'readonly',
+          describe: 'readonly',
+          it: 'readonly',
+          expect: 'readonly',
+          beforeEach: 'readonly',
+        },
       },
-      sourceType: 'module',
+      rules: {
+        'no-console': 'off', 
+      },
     },
-    plugins: {
-      import: eslintPluginImport,
-      cypress: eslintPluginCypress,
-    },
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-undef': 'error',
-      'import/no-unresolved': 'off',
-    },
-  },
-];
+  ]
+  
